feeae4c22759574fc275305aae9090c63789cbc9
dom-hydracor-intern <domonique.walcott@hydracor.net>
2021-05-19 16:12:24 -0400

    include addArticle function

--- js/fetch.js
+++ js/fetch.js
@@ -4,15 +4,17 @@ var userData = {
 };
 
 
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEzLCJpYXQiOnsiZGF0ZSI6IjIwMjEtMDUtMTggMTc6MzU6MjguNDcwMTYwIiwidGltZXpvbmVfdHlwZSI6MywidGltZXpvbmUiOiJVVEMifSwiZXhwIjoxNjIxNzA0OTI4fQ.li0p7X4O8q3ElU1QvCVfsbBYLYiBVf8xSkDXGg0GDH0';
+
 
 //get token
 
-fetch('http://206.189.202.188:2593/api/users/add', {
-    method: 'POST',
-    headers: {
-        "Accept": "application/json"
-      },
-    body: JSON.stringify(userData),
+fetch('http://206.189.202.188:2593/api/users/token', {
+    method: 'GET',
+    headers:{
+        "Accept": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
   })
     .then(response => {
         if (response.ok) {
@@ -25,17 +27,66 @@ fetch('http://206.189.202.188:2593/api/users/add', {
 // write JSON object for an article { id:, title, body, category_id}
 
 
-
+/*
 
 // use token to add article
 
-const thisendData = {}
+let newArticle = {
+    id: "",
+    title: "",
+    body: "",
+    categpry_id: "",
+    created: "",
+    modified: ""
+}
 
+
+const getArticles = () => {
+    fetch('http://206.189.202.188:2593/articles/add', {
+    method: 'POST',
+    headers: {
+        "Accept": "application/json",
+        "Authorization": "token ${token}", 
+        "Content-Type": "application/json"
+      },
+    body: JSON.stringify(newArticle),
+  });
+};
 
 
 // use token to delete article
 
-const sendData = {}
+
+fetch('http://206.189.202.188:2593/api/articles/add', {
+    method: 'POST',
+    headers: {
+        "Accept": "application/json",
+        "Authorization": "token ${token}", 
+     "Content-Type": "application/json"
+      },
+    body: JSON.stringify(newArticle),
+  })
+  .then(response => {
+    if (response.ok) {
+        response.json().then(json => { console.log(json);
+        });
+    }
+});
+
+
+ use token to view article
+
+fetch('http://206.189.202.188:2593/articles', {
+    method: 'GET',
+    headers: {
+        "Accept": "application/json"
+      },
+    body: JSON.stringify(newArticle),
+  })
+
+*/
+
+// use token to view article
 
 
 
